530763a6eba689eb6a2dac0d649d87e5b6d30696
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-12 07:12:01 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,15 +6,13 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    if (BRANDS.include? @brand)
+      BRANDS << brand 
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-
-  def brands=(brand)
-    @brands = brand
-    BRANDS << brand 
   end
 end
